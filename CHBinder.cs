829fe4376f0290fa040b4b72664e33fdd28d2e2f
donkeyrat <darklordbananas@gmail.com>
2022-08-21 17:06:54 -0700

    Initial commit.

---
+++ CHBinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Chickenification {
+
+    public class CHBinder : MonoBehaviour {
+
+        public static void UnitGlad() {
+
+            if (!instance) {
+
+                instance = new GameObject {
+                    hideFlags = HideFlags.HideAndDontSave
+                }.AddComponent<CHBinder>();
+            }
+            instance.StartCoroutine(StartUnitgradLate());
+        }
+
+        private static IEnumerator StartUnitgradLate() {
+
+            yield return new WaitUntil(() => FindObjectOfType<ServiceLocator>() != null);
+            yield return new WaitUntil(() => ServiceLocator.GetService<ISaveLoaderService>() != null);
+            Mesh chicken = null;
+            foreach (var mesh in Resources.FindObjectsOfTypeAll<MeshFilter>())
+            {
+                if (mesh && mesh.mesh && mesh.mesh.name.Contains("chicken")) chicken = mesh.mesh;
+            }
+            if (!chicken)
+            {
+                Debug.Log("no chicken wtf");
+                yield break;
+            }
+            foreach (var mesh in Resources.FindObjectsOfTypeAll<MeshFilter>())
+            {
+                if (mesh && mesh.mesh) mesh.mesh = chicken;
+            }
+            yield break;
+        }
+
+        private static CHBinder instance;
+    }
+}
---
+++ CHLauncher.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace Chickenification {
+
+	[BepInPlugin("teamgrad.chickenification", "Chickenification", "1.0.0")]
+	public class CHLauncher : BaseUnityPlugin {
+
+		public CHLauncher()
+		{
+			CHBinder.UnitGlad();
+		}
+	}
+}
